1363057dd722f50bd4f9e5153eae2bf7930b02a2
Etornam Sunu <sunumacbright@gmail.com>
2020-04-03 09:20:25 +0000

    Update custom_button_widget.dart

--- lib/shared_widgets/custom_button_widget.dart
+++ lib/shared_widgets/custom_button_widget.dart
@@ -4,41 +4,41 @@ import 'package:flutter/material.dart';
 class CustomButtomWidget extends StatelessWidget {
   const CustomButtomWidget({
     Key key,
-    @required this.util,
     @required this.text,
     @required this.isHasIcon,
     this.icon,
-    @required this.color,
     @required this.onTap,
+    @required this.textColor,
+    @required this.backgroundColor,
   }) : super(key: key);
 
-  final Util util;
+  final Color textColor;
   final String text;
   final bool isHasIcon;
   final IconData icon;
-
-  ///added color and ontap function and reference them in button code
-  final Color color;
+  final Color backgroundColor;
   final Function onTap;
 
   @override
   Widget build(BuildContext context) {
     return FlatButton(
-      padding: const EdgeInsets.all(12.0),
-        color: Colors.white,
+        padding: const EdgeInsets.all(12.0),
+        color: backgroundColor,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
         onPressed: onTap,
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            isHasIcon ? Icon(icon, color: color) : SizedBox(),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 7,
+            isHasIcon ? Icon(icon, color: textColor) : SizedBox(),
+            Center(
+              child: Text(text,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1
+                      .copyWith(color: textColor)),
             ),
-            Text(text,
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: util.mainColor))
+            SizedBox()
           ],
         ));
   }
